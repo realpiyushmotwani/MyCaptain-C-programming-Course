131020519229f12baf376684131d2e44ffea0d0f
realpiyushmotwani <100508072+realpiyushmotwani@users.noreply.github.com>
2022-02-27 16:20:36 +0530

    Update SwapwithoutTemp.c

--- SwapwithoutTemp.c
+++ SwapwithoutTemp.c
@@ -2,10 +2,10 @@
  
 int main()
 {
-   int num1, num2; //declaring temporary variable
+   int num1, num2; //declaring 2 numbers of type int 
  
-   printf("\nEnter the value of the first number\n");
-   scanf("%d", &num1);
+   printf("\nEnter the value of the first number\n"); //printf to ask user to input values
+   scanf("%d", &num1); //scanf to input values
    printf("Enter the value of the second number\n");
    scanf("%d", &num2);
  
@@ -13,11 +13,11 @@ int main()
    printf("first number = %d\n", num1);
    printf("second number = %d\n", num2);
    printf("\n");
-   num1 = num1 + num2;
+   num1 = num1 + num2; //swapping without temp variable
    num2 = num1- num2;
    num1 = num1 - num2;
  
-   printf("After swapping, the numbers become :  \n");
+   printf("After swapping, the numbers become :  \n"); //displaying the required output
    printf("first number becomes = %d\n", num1);
    printf("second number becomes = %d\n", num2);
    printf("\n");
